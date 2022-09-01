1e007f301864117db9de75a126b6887681aac96f
Kyle Butts <buttskyle96@gmail.com>
2022-09-01 10:36:14 -0600

    Initial results page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
 
-import react from "@astrojs/react";
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
-});
+	integrations: [tailwind(), react()],
+	vite: {
+		ssr: {
+			external: ["svgo"],
+		},
+	},
+})
